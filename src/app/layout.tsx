508c23c8f2dbfb2c98b07b9836fcdf8dd93a4289
EllyBax <baxllycc@gmail.com>
2025-04-18 17:41:26 +0300

    defined color scheme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 
 import { type Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Courier_Prime, Geist } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -9,17 +9,18 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-const geist = Geist({
+const courier = Courier_Prime({
+  weight: ["400", "700"],
   subsets: ["latin"],
-  variable: "--font-geist-sans",
+  variable: "--font-courier-prime",
 });
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${courier.variable}`}>
+      <body className="min-h-screen bg-primary text-text-main">{children}</body>
     </html>
   );
 }
